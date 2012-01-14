686f648da5575df481caf9b6f96cc132e096f779
Aaron Faber <shnist@btinternet.com>
2012-01-14 17:58:06 +0000

    added common.js file and wrote the set up for it

---
+++ assets/scripts/common.js
@@ -0,0 +1,70 @@
+/*!
+    Boonbox
+    Common JavaScript for Boonbox sites
+
+    JSDoc notation
+    
+    @version 0.1
+    @author Boonbox - http://www.boonbox.co.uk
+    @requires jQuery Core 1.7+ - http://www.jquery.com/
+*/
+
+/**
+ * @namespace Namespace for all common functionality for the site 
+ * @memberOf boonbox
+ */
+
+(function () {
+	var Boonbox = {
+		
+		/**
+		 * Extends the Boonbox object with the intended object
+		 * @function
+		 * @memberOf Boonbox
+		 * @name {String} The namespace the object should be dropped into
+		 * @obj {Object} The object
+		 */
+		extend: function (name, object){
+			
+			if (this[name] === undefined){
+				this[name] = object;
+			}
+			
+		}
+	};
+	
+	Boonbox = window.Boonbox;
+	
+}());
+
+Boonbox.extend('common', {
+	/**
+	 * Initialiser of common functionality across the site
+	 * @function
+	 * @memberOf boonbox
+	 */
+	init : function(){
+		
+	}
+
+});
+
+Boonbox.extend('filters', {
+	/**
+	 * Initialiser of the setup functionality of the filters
+	 * @function
+	 * @memberOf boonbox
+	 */
+	init : function (){}
+
+});
+
+
+/**
+ * Document ready function for jquery
+ */
+
+$(document).ready(function () {
+	Boonbox.common.init();
+	Boonbox.filters.init();
+});
