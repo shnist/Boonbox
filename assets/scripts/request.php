ae93b39c5d158bb96c77fb7b0a6ad48aa28f7d16
Aaron Faber <shnist@btinternet.com>
2012-02-10 16:34:39 +0000

    removed changes to request.php

--- assets/scripts/request.php
+++ assets/scripts/request.php
@@ -1,10 +1,19 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
 
-$json = array(array('name'=>'Monster Toys', 'price'=>'25.99', 'rating'=>'4', 'image'=>'../../assets/images/placeHolder1.jpg'), array('name'=>'Mina Nishimura', 'price'=>'25.99', 'rating'=>'4', 'image'=>'../../assets/images/placeHolder1.jpg'),array('name'=>'Johanna Elise Oja', 'price'=>'25.99', 'rating'=>'4', 'image'=>'../../assets/images/placeHolder1.jpg'), array('name'=>'Aaron Faber', 'price'=>'25.99', 'rating'=>'4', 'image'=>'../../assets/images/placeHolder1.jpg'), array('name'=>'Beth Williams', 'price'=>'25.99', 'rating'=>'4', 'image'=>'../../assets/images/placeHolder1.jpg'), array('name'=>'Kingsley', 'price'=>'25.99', 'rating'=>'4', 'image'=>'../../assets/images/placeHolder1.jpg'), array('name'=>'Laura', 'price'=>'25.99', 'rating'=>'4', 'image'=>'../../assets/images/placeHolder1.jpg'), array('name'=>'Monster Toys', 'price'=>'25.99', 'rating'=>'4', 'image'=>'../../assets/images/placeHolder1.jpg'));
+$json = array(
+		  array('name'=>'Monster Toys', 'price'=>'25.99', 'rating'=>'4', 'image'=>'../../assets/images/placeHolder1.jpg'),
+		  array('name'=>'Mina Nishimura', 'price'=>'25.99', 'rating'=>'4', 'image'=>'../../assets/images/placeHolder1.jpg'),
+		  array('name'=>'Johanna Elise Oja', 'price'=>'25.99', 'rating'=>'4', 'image'=>'../../assets/images/placeHolder1.jpg'),
+		  array('name'=>'Aaron Faber', 'price'=>'25.99', 'rating'=>'4', 'image'=>'../../assets/images/placeHolder1.jpg'),
+		  array('name'=>'Beth Williams', 'price'=>'25.99', 'rating'=>'4', 'image'=>'../../assets/images/placeHolder1.jpg'),
+		  array('name'=>'Kingsley', 'price'=>'25.99', 'rating'=>'4', 'image'=>'../../assets/images/placeHolder1.jpg'),
+		  array('name'=>'Laura', 'price'=>'25.99', 'rating'=>'4', 'image'=>'../../assets/images/placeHolder1.jpg'),
+		  array('name'=>'Monster Toys', 'price'=>'25.99', 'rating'=>'4', 'image'=>'../../assets/images/placeHolder1.jpg')
+		);
 
 // simulates delay - in seconds
 sleep(4);
 $jsonFinal = json_encode($json);
-//echo $jsonFinal;
+echo $jsonFinal;
 ?>
